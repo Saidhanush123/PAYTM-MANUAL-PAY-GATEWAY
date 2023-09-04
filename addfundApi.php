a1167b8aaacd3c7e69b989f6924c59203b07412c
GitCodex <900mahata@gmail.com>
2023-09-05 01:21:29 +0530

    All src code upload
    I just create an API using response data of paytm api and return back json data simple

---
+++ addfundApi.php
@@ -0,0 +1,28 @@
+<?php 
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+$data = json_decode(file_get_contents("php://input"), true);
+$txn_id = $data['txn-id'];
+$txn_amount = $data['txn-amount'];
+
+include "encdec_paytm.php";
+
+//$icid = "T2308271602462115962313";
+
+//$TXN_AMOUNT = "100";
+
+$responseParamList = array();
+
+$requestParamList = array();
+
+$requestParamList = array("MID" => "lrljVN14090197822476" , "ORDERID" => $txn_id); 
+
+$responseParamList = getTxnStatusNew($requestParamList);
+
+
+if (01 == $responseParamList["RESPCODE"]) {
+    echo json_encode($responseParamList);
+}else {
+    echo json_encode(array('message' => 'Failed to add Fund', 'status' => false));
+}
+?>
